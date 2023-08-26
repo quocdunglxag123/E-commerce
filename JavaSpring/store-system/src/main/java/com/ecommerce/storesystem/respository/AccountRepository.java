924dbbed439c5c3f095a1701b06e57447bc01873
Ly Quoc Dung <quocdunglxag123@gmail.com>
2023-08-26 23:25:58 +0700

    revert first commit

--- JavaSpring/store-system/src/main/java/com/ecommerce/storesystem/respository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.storesystem.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.storesystem.entity.AccountEntity;
-
-public interface AccountRepository extends JpaRepository<AccountEntity, Long>{
-	AccountEntity findByUserName(String userName);
-}
--- JavaSpring/store-system/src/main/java/com/ecommerce/storesystem/service/AccountService.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.storesystem.service;
-
-import com.ecommerce.storesystem.dto.AccountDto;
-
-public interface AccountService {
-	Object checkAccount(AccountDto accountDto); 
-}
