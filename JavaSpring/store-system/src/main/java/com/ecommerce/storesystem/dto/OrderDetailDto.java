44b0b99ba7b676601c75fd9b5369794b18cbaec7
Ly Quoc Dung <quocdunglxag123@gmail.com>
2023-09-12 02:01:31 +0700

    Dung-lq : fix product in order

---
+++ JavaSpring/store-system/src/main/java/com/ecommerce/storesystem/dto/OrderDetailDto.java
@@ -0,0 +1,122 @@
+package com.ecommerce.storesystem.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class OrderDetailDto extends BaseDto {
+	private boolean paid;
+
+	private String phone;
+
+	private String address;
+
+	private String fullName;
+
+	private List<ProductDto> product;
+
+	private Long accountId;
+	
+	private Long statusId;
+	
+	private Date startDelivery;
+	
+	private Date endDelivery;
+	
+	public OrderDetailDto() {
+		super();
+	}
+
+	public OrderDetailDto(boolean paid, String phone, String address, String fullName, List<ProductDto> product,
+			Long accountId, Long statusId, Date startDelivery, Date endDelivery) {
+		super();
+		this.paid = paid;
+		this.phone = phone;
+		this.address = address;
+		this.fullName = fullName;
+		this.product = product;
+		this.accountId = accountId;
+		this.statusId = statusId;
+		this.startDelivery = startDelivery;
+		this.endDelivery = endDelivery;
+	}
+
+	public boolean isPaid() {
+		return paid;
+	}
+
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public List<ProductDto> getProduct() {
+		return product;
+	}
+
+	public void setProduct(List<ProductDto> productDto) {
+		this.product = productDto;
+	}
+	
+	public void addProductToListDto(ProductDto productDto) {
+		this.product.add(productDto);
+	}
+
+	public Long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+
+	public Long getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(Long statusId) {
+		this.statusId = statusId;
+	}
+
+	public Date getStartDelivery() {
+		return startDelivery;
+	}
+
+	public void setStartDelivery(Date startDelivery) {
+		this.startDelivery = startDelivery;
+	}
+
+	public Date getEndDelivery() {
+		return endDelivery;
+	}
+
+	public void setEndDelivery(Date endDelivery) {
+		this.endDelivery = endDelivery;
+	}
+
+	
+	
+
+}
--- JavaSpring/store-system/src/main/java/com/ecommerce/storesystem/service/impl/OrderServiceImpl.java
+++ JavaSpring/store-system/src/main/java/com/ecommerce/storesystem/service/impl/OrderServiceImpl.java
@@ -1,14 +1,24 @@
 package com.ecommerce.storesystem.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+
+import com.ecommerce.storesystem.dto.OrderDetailDto;
 import com.ecommerce.storesystem.dto.OrderDto;
+import com.ecommerce.storesystem.dto.ProductDto;
 import com.ecommerce.storesystem.entity.OrderEntity;
+import com.ecommerce.storesystem.entity.ProductEntity;
 import com.ecommerce.storesystem.entity.StatusEntity;
 import com.ecommerce.storesystem.exception.OrderException;
+import com.ecommerce.storesystem.exception.ProductException;
 import com.ecommerce.storesystem.mapstruct.OrderMapper;
+import com.ecommerce.storesystem.mapstruct.ProductMapper;
 import com.ecommerce.storesystem.respository.OrderRepository;
+import com.ecommerce.storesystem.respository.ProductRepository;
 import com.ecommerce.storesystem.respository.StatusRepository;
 import com.ecommerce.storesystem.service.OrderService;
 
@@ -16,59 +26,132 @@ import com.ecommerce.storesystem.service.OrderService;
 public class OrderServiceImpl implements OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private ProductMapper productMapper;
+
 	@Autowired
 	private OrderMapper orderMapper;
-	
+
 	@Autowired
 	private StatusRepository statusRepository;
-	
+
 	@Autowired
 	private Environment define;
-	
+
 	@Autowired
-	public OrderServiceImpl(OrderMapper orderMapper, OrderRepository orderRepository) {
+	public OrderServiceImpl(OrderMapper orderMapper, OrderRepository orderRepository, ProductMapper productMapper) {
 		this.orderMapper = orderMapper;
+		this.productMapper = productMapper;
 		this.orderRepository = orderRepository;
 	}
-	
+
 	@Override
 	public Object getAllOrder() {
-		return orderMapper.ListOrderEntityToListOrderDto(orderRepository.findAll());
+		List<OrderEntity> listOrderEntity = orderRepository.findAll();
+		List<OrderDetailDto> listOrderDetailDto = new ArrayList<OrderDetailDto>();
+		for (OrderEntity orderEntity : listOrderEntity) {
+			OrderDetailDto orderDetailDto = new OrderDetailDto();
+			orderDetailDto.setAccountId(orderEntity.getAccountId());
+			orderDetailDto.setAddress(orderEntity.getAddress());
+			orderDetailDto.setCreateDate(orderEntity.getCreateDate());
+			orderDetailDto.setEndDelivery(orderEntity.getEndDelivery());
+			orderDetailDto.setFullName(orderEntity.getFullName());
+			orderDetailDto.setId(orderEntity.getId());
+			orderDetailDto.setPaid(orderEntity.isPaid());
+			orderDetailDto.setPhone(orderEntity.getPhone());
+			orderDetailDto.setServiceCall("");
+			orderDetailDto.setStartDelivery(orderEntity.getStartDelivery());
+			orderDetailDto.setStatusId(orderEntity.getStatusId());
+			orderDetailDto.setUpdateDate(orderEntity.getUpdateDate());
+			String productIdsNoWhiteSpace = orderEntity.getProductIds().replaceAll("\\s", "");
+			String[] productIds = productIdsNoWhiteSpace.split(",");
+
+			List<ProductDto> listproductDtos = new ArrayList<ProductDto>();
+
+			for (String productId : productIds) {
+				ProductEntity productEntity = productRepository.findOneById(Long.parseLong(productId));
+				if (productEntity == null) {
+					throw new ProductException("Product Id: " + productId + " Not Found!");
+				}
+				ProductDto productDto = productMapper.productEntityToProductDto(productEntity);
+				listproductDtos.add(productDto);
+
+			}
+			orderDetailDto.setProduct(listproductDtos);
+			listOrderDetailDto.add(orderDetailDto);
+		}
+		return listOrderDetailDto;
 	}
 
 	@Override
 	public Object getOrder(Long id) {
-		return orderMapper.orderEntityToOrderDto(orderRepository.findOneById(id));
+
+		OrderEntity orderEntity = orderRepository.findOneById(id);
+		OrderDetailDto orderDetailDto = new OrderDetailDto();
+		orderDetailDto.setAccountId(orderEntity.getAccountId());
+		orderDetailDto.setAddress(orderEntity.getAddress());
+		orderDetailDto.setCreateDate(orderEntity.getCreateDate());
+		orderDetailDto.setEndDelivery(orderEntity.getEndDelivery());
+		orderDetailDto.setFullName(orderEntity.getFullName());
+		orderDetailDto.setId(orderEntity.getId());
+		orderDetailDto.setPaid(orderEntity.isPaid());
+		orderDetailDto.setPhone(orderEntity.getPhone());
+		orderDetailDto.setServiceCall("");
+		orderDetailDto.setStartDelivery(orderEntity.getStartDelivery());
+		orderDetailDto.setStatusId(orderEntity.getStatusId());
+		orderDetailDto.setUpdateDate(orderEntity.getUpdateDate());
+		String productIdsNoWhiteSpace = orderEntity.getProductIds().replaceAll("\\s", "");
+		String[] productIds = productIdsNoWhiteSpace.split(",");
+
+		List<ProductDto> listproductDtos = new ArrayList<ProductDto>();
+
+		for (String productId : productIds) {
+			ProductEntity productEntity = productRepository.findOneById(Long.parseLong(productId));
+			if (productEntity == null) {
+				throw new ProductException("Product Id: " + productId + " Not Found!");
+			}
+			ProductDto productDto = productMapper.productEntityToProductDto(productEntity);
+			listproductDtos.add(productDto);
+
+		}
+		orderDetailDto.setProduct(listproductDtos);
+		return orderDetailDto;
 	}
 
 	@Override
 	public Object addOrder(OrderDto orderDto) {
-		//Get statusEntity
+		// Get statusEntity
 		StatusEntity statusEntity = statusRepository.findByStatusName(define.getProperty("statusNameShoppingCart"));
-		
-		//Check duplicate Shopping Cart (one account- one shopping cart)
-		Long countOrderIsNotShipping = orderRepository.findByStatusIdAndAccountId(statusEntity.getId(),orderDto.getAccountId());
-		if(countOrderIsNotShipping != 0) {
-			throw new OrderException("Account Id: "+orderDto.getAccountId()+" Cannot Have More Than Two Order (Shopping Cart) With Status Id: 1");
+
+		// Check duplicate Shopping Cart (one account- one shopping cart)
+		Long countOrderIsNotShipping = orderRepository.findByStatusIdAndAccountId(statusEntity.getId(),
+				orderDto.getAccountId());
+		if (countOrderIsNotShipping != 0) {
+			throw new OrderException("Account Id: " + orderDto.getAccountId()
+					+ " Cannot Have More Than Two Order (Shopping Cart) With Status Id: 1");
 		}
-		
-		//add order for shopping cart 
+
+		// add order for shopping cart
 		OrderEntity orderEntity = orderMapper.orderDtoToOrderEntity(orderDto);
 		orderEntity.setStatusId(statusEntity.getId());
 		orderRepository.save(orderEntity);
 		return true;
-		
+
 	}
-	
+
 	@Override
 	public Object updateOrder(OrderDto orderDto) {
-		//Not permit update order is already to shipping
-		if(orderDto.getStatusId() != 1) {
-			throw new OrderException("Order id: "+orderDto.getId()+" is Shipping With statusId: "+ orderDto.getStatusId());
+		// Not permit update order is already to shipping
+		if (orderDto.getStatusId() != 1) {
+			throw new OrderException(
+					"Order id: " + orderDto.getId() + " is Shipping With statusId: " + orderDto.getStatusId());
 		}
-		
-		//Update order
+
+		// Update order
 		OrderEntity orderEntityUpdate = orderRepository.findOneById(orderDto.getId());
 		if (orderEntityUpdate != null) {
 			orderEntityUpdate.setOrderEntity(orderDto);
@@ -83,5 +166,5 @@ public class OrderServiceImpl implements OrderService {
 		orderRepository.deleteById(id);
 		return true;
 	}
-	
+
 }
